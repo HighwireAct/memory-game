dcc3e15d1bbb6601302c18b0fdb854b835663e84
Ethan Smoller <ethansmoller@gmail.com>
2019-02-17 02:55:19 -0800

    Updates

---
+++ precache-manifest.3af32e1d4cbfabb88f22f41d1b677719.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba4445eae776a971e5e626c4c54915aa",
+    "url": "/memory-game/static/media/new-record.ba4445ea.wav"
+  },
+  {
+    "revision": "c2bf92c6c15f5c5ad92fce1c8cc48b5c",
+    "url": "/memory-game/static/media/loss.c2bf92c6.wav"
+  },
+  {
+    "revision": "435a81855f49d55acd25",
+    "url": "/memory-game/static/js/runtime~main.435a8185.js"
+  },
+  {
+    "revision": "506c80910a8b4f5f7c16",
+    "url": "/memory-game/static/js/main.506c8091.chunk.js"
+  },
+  {
+    "revision": "6cdbe0893240bda39b09",
+    "url": "/memory-game/static/js/2.6cdbe089.chunk.js"
+  },
+  {
+    "revision": "506c80910a8b4f5f7c16",
+    "url": "/memory-game/static/css/main.d234e4ec.chunk.css"
+  },
+  {
+    "revision": "2dca25c00b8998d46c710a3fd58fa947",
+    "url": "/memory-game/index.html"
+  }
+];
