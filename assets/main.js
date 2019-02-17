dd48238fd2772cb6e079cd3115e6e9e45d2e98f8
Ethan Smoller <ethansmoller@gmail.com>
2019-02-17 03:23:07 -0800

    Updates

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,6 @@
 const bgMusic = new Audio('./assets/main-theme.mp3');
+const chooseYourCharacter = new Audio('./assets/choose-your-character.wav');
 bgMusic.loop = true;
 
+chooseYourCharacter.play();
 bgMusic.play();
